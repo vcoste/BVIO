3ad1bf5cd1a3a1a1239bf0f144cf781fbf6330ab
Vincent Coste <vincent.coste@bazaarvoice.com>
2014-04-03 13:51:07 -0500

    started creating category tree json

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,14 @@
 class Category < ActiveRecord::Base
+
+	has_many :children, :class_name => "Category", :foreign_key => "parent_id"
+	belongs_to :parent, :class_name => "Category"
+
+	def self.to_tree
+	puts @children
+    {
+    	"category_id" => @category_id,
+      	"subCategory"   => @children.map { |c| c.to_tree }
+    }
+  end
+
 end
