629aad19a6a06a232e8420432cf3e9e0a15817b4
Vincent Coste <vincent.coste@bazaarvoice.com>
2014-04-03 18:55:26 -0500

    more changes to the map

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -2,23 +2,50 @@ tomtom.apiKey = "cqz42jgvsqt6qra52jj373hr";
 // tomtom.setImagePath("/images/map");
 
 $( document ).ready(function() {
-
+	var map
 	function getLocation() {
-		console.log('hey')
 		if (navigator.geolocation) {
 			navigator.geolocation.getCurrentPosition(displayMap);
 	    }
 	}
 
-	function displayMap() {
-		var map = new tomtom.Map({
+	function getCornerCoordinates(map) {
+		var bounds = map.getBounds();
+		console.log(bounds);
+		var corners = {
+			'NE' : {
+				'lat': bounds._northEast.lat,
+				'lng': bounds._northEast.lng
+			},
+			'SE': {
+				'lat': bounds._southWest.lat,
+				'lng': bounds._northEast.lng
+			}, 
+			'SW': {
+				'lat': bounds._southWest.lat,
+				'lng': bounds._southWest.lng
+			}, 
+			'NW': {
+				'lat': bounds._northEast.lat,
+				'lng': bounds._southWest.lng
+			}
+		}
+		return corners;
+	}
+
+	function displayMap(position) {
+		map = new tomtom.Map({
 			domNode: "map",
-			apiKey: tomtom.apiKey
+			center: [position.coords.latitude, position.coords.longitude],
+			apiKey: tomtom.apiKey,
+			zoom: 14,
+			overviewMap: true,
+			scale: true,
+			panZoomBar: true
 		});
+
+		console.log(getCornerCoordinates(map));
 	}
 
 	getLocation();
-	// map.on("load", function() {
-	// 	alert("MAP Loaded");
-	// });
 });
