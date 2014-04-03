38c9da71020edbb3c0184e1f15dda6800b205d34
Vincent Coste <vincent.coste@bazaarvoice.com>
2014-04-02 21:35:55 -0500

    readme updated and other files added

--- README.md
+++ README.md
@@ -2,3 +2,11 @@ BVIO
 ====
 
 BVIO hackathon project
+
+Install & Run
+=============
+
+npm install
+node server
+
+Then checkout http://localhost:8080/
--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var http = require('http');
 http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
     response.end('Hello World\n');
-}).listen(8080);
+}).listen(3000);
 
-console.log('Server started');
+console.log('waiting for ever');
