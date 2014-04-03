19b4fe1f80eca3f3bce19181a719a205930ee54c
Vincent Coste <vincent.coste@bazaarvoice.com>
2014-04-03 14:50:45 -0500

    conflict resolution

--- app/models/category.rb
+++ app/models/category.rb
@@ -5,10 +5,16 @@ class Category < ActiveRecord::Base
   has_many :products
 
 	def to_tree
-    {
-    	"category_id" => self.category_id,
-      "subCategory"   => self.children.map { |c| c.to_tree }
-    }
+	    {
+	    	"id" => self.id,
+	    	"category_page_url" => self.category_page_url,
+			"category_id" => self.category_id,
+			"image_url" => self.image_url,
+			"name" => self.name,
+			"parent_id_str" => self.parent_id_str,
+			"parent_id" => self.parent_id,
+		    "subCategory"  => self.children.map { |c| c.to_tree }
+	    }
   end
 
   def get_top_products_for_area(t_left, t_right, b_left, b_right)
