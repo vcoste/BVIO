c71f79c73e8b28a9003ed42d53880e0429435592
Vincent Coste <vincent.coste@bazaarvoice.com>
2014-04-03 23:48:17 -0500

    loading spinner added

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -5,6 +5,9 @@
 	var currentCoordinates;
 	var category_id = 35;
 
+	var mapContainer;
+	var spinner;
+
 	function getLocation(cb) {
 		if (navigator.geolocation) {
 			navigator.geolocation.getCurrentPosition(cb);
@@ -46,7 +49,7 @@
 			scale: true,
 			panZoomBar: true
 		});
-
+		mapSpinner.stop();
 		initMarkers(map);
 		getProducts(category_id, map);
 	}
@@ -97,6 +100,27 @@
 	$(function () {
 		tomtom.apiKey = "cqz42jgvsqt6qra52jj373hr";
 		tomtom.setImagePath("../../../vendor/assets/map");
+
+		var opts = {
+		  lines: 13, // The number of lines to draw
+		  length: 20, // The length of each line
+		  width: 10, // The line thickness
+		  radius: 30, // The radius of the inner circle
+		  corners: 1, // Corner roundness (0..1)
+		  rotate: 0, // The rotation offset
+		  direction: 1, // 1: clockwise, -1: counterclockwise
+		  color: '#000', // #rgb or #rrggbb or array of colors
+		  speed: 1, // Rounds per second
+		  trail: 60, // Afterglow percentage
+		  shadow: false, // Whether to render a shadow
+		  hwaccel: false, // Whether to use hardware acceleration
+		  className: 'spinner', // The CSS class to assign to the spinner
+		  zIndex: 2e9 // The z-index (defaults to 2000000000)
+		};
+		
+		mapContainer = document.getElementById('map-container');
+		mapSpinner = new Spinner(opts).spin(mapContainer);
+
 
 		$("#reDoSearch").bind( "click", function() {
 		  mapCoordinates = getCornerCoordinates(map)
