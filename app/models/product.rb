a09f97f2128a779cad6915d4e96e8cfa1b9e9d8f
Tony Cui <tony.cui@bazaarvoice.com>
2014-04-04 02:04:19 -0500

    turns out i cant math

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,9 +6,10 @@ class Product < ActiveRecord::Base
   def get_reviews_by_area(t_left, t_right, b_left, b_right)
     reviews = self.reviews
     reviews = reviews.select{|r|
-      r.latitude >= t_right['latitude'].to_f and r.latitude <= b_right['latitude'].to_f and r.latitude >= t_left['latitude'].to_f and r.latitude <= b_left['latitude'].to_f and
+      r.latitude <= t_right['latitude'].to_f and r.latitude >= b_right['latitude'].to_f and r.latitude <= t_left['latitude'].to_f and r.latitude >= b_left['latitude'].to_f and
       r.longitude <= t_right['longitude'].to_f and r.longitude >= t_left['longitude'].to_f and r.longitude >= b_left['longitude'].to_f and r.longitude <= b_right['longitude'].to_f
     }
+    puts reviews.length
     reviews
   end
 
