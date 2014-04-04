f2aa29bc4ce0f2cab2b07404d3208d07c0a76862
Tony Cui <tony.cui@bazaarvoice.com>
2014-04-03 23:15:18 -0500

    test

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,8 +6,8 @@ class Product < ActiveRecord::Base
   def get_reviews_by_area(t_left, t_right, b_left, b_right)
     reviews = self.reviews
     reviews.select!{|r|
-      r.latitude >= t_right['latitude'] and r.latitude <= b_right['latitude'] and r.latitude >= t_left['latitude'] and r.latitude <= b_left['latitude'] and
-      r.longitude <= t_right['longitude'] and r.longitude >= t_left['longitude'] and r.longitude >= b_left['longitude'] and r.longitude <= b_right['longitude']
+      r.latitude >= t_right['latitude'].to_f and r.latitude <= b_right['latitude'].to_f and r.latitude >= t_left['latitude'].to_f and r.latitude <= b_left['latitude'].to_f and
+      r.longitude <= t_right['longitude'].to_f and r.longitude >= t_left['longitude'].to_f and r.longitude >= b_left['longitude'].to_f and r.longitude <= b_right['longitude'].to_f
     }
     puts reviews.length
     reviews
